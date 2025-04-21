f236d29a6c4a2d78bd3dcad0cbf09822810da1c6
Focus_Fox <iammukulsharma1@gmail.com>
2025-04-21 15:29:11 +0530

    Client

--- src/component/RecommendedFood.jsx
+++ src/component/RecommendedFood.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaHeart, FaStar } from 'react-icons/fa'
+import { useFoodContext } from '../../context/foodContext'
+import Foods from './Foods'
+import axios from 'axios'
 
 const RecommendedFood = () => {
+    const [ratedfood, setratedFoood] = useState([])
+    const { food, setFood } = useFoodContext()
+    const getFoods = async () => {
+        try {
+            const res = await axios.get(`http://localhost:8000/api/v1/food/getToRated`)
+            if (res.data.success) {
+                setratedFoood(res.data.data.food)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getFoods()
+    }, [ratedfood])
     return (
         <div className="py-3 px-10 sm:px-4 md:px-6 lg:px-6">
             <div className="container mx-auto py-[2vh]">
@@ -9,118 +28,9 @@ const RecommendedFood = () => {
                     Recommendate <span className="text-[#f54748]">Foods</span>
                 </div>
                 <div className="grid py-6 gap-8 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1">
-                    <div className="food-card bg-red-500/10  rounded-xl flex flex-col cursor-pointer items-center p-5">
-                        <div className="relative mb-3">
-                            <img src="https://i.ibb.co/jMxb2GC/i5.png" alt="" srcset="" />
-                            <div className="absolute top-2 left-2">
-                                <button className="shadow-sm text-white bg-red-500 hover:bg-red-700 cursor-pointer p-5 rounded-full relative">
-                                    <FaHeart className=' absolute text-xl top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ' />
-                                </button>
-                            </div>
-
-                            <div className=" absolute bottom-2 right-2">
-                                <button className=" shadow-sm bottom-4 border-white text-white bg-[#fdc55e] cursor-pointer p-3 h-14 w-14 text-xl font-bold rounded-full relative">
-                                    <div className=" absolute text-xl top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">$10</div>
-                                </button>
-                            </div>
-                        </div>
-                        <div className="flex gap-4 items-center">
-                            <p className="text-xl text-center font-bold text-[#f54748]">
-                                Kebab
-                            </p>
-                            <div className="flex text-sm space-x-2 cursor-pointer">
-                                <span className=" font-normal text-[#fdc55e]">4.3</span>
-                                <FaStar size={16} className='text-[#fdc55e]' />
-                                <span className=" font-medium">(4)</span>
-                            </div>
-
-                        </div>
-                        <button className=" bg-[#f54748] active:scale-90 transition duration-150 transform hover:shadow-xl shadow-md rounded-full px-8 py-2 text-xl font-medium text-white"> order now</button>
-                    </div>
-                    <div className="food-card flex bg-red-500/10  rounded-xl flex-col cursor-pointer items-center p-5">
-                        <div className="relative mb-3">
-                            <img src="https://i.ibb.co/jMxb2GC/i5.png" alt="" srcset="" />
-                            <div className="absolute top-2 left-2">
-                                <button className="shadow-sm text-white bg-red-500 hover:bg-red-700 cursor-pointer p-5 rounded-full relative">
-                                    <FaHeart className=' absolute text-xl top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ' />
-                                </button>
-                            </div>
-
-                            <div className=" absolute bottom-2 right-2">
-                                <button className=" shadow-sm bottom-4 border-white text-white bg-[#fdc55e] cursor-pointer p-3 h-14 w-14 text-xl font-bold rounded-full relative">
-                                    <div className=" absolute text-xl top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">$10</div>
-                                </button>
-                            </div>
-                        </div>
-                        <div className="flex gap-4 items-center">
-                            <p className="text-xl text-center font-bold text-[#f54748]">
-                                Kebab
-                            </p>
-                            <div className="flex text-sm space-x-2 cursor-pointer">
-                                <span className=" font-normal text-[#fdc55e]">4.3</span>
-                                <FaStar size={16} className='text-[#fdc55e]' />
-                                <span className=" font-medium">(4)</span>
-                            </div>
-
-                        </div>
-                        <button className=" bg-[#f54748] active:scale-90 transition duration-150 transform hover:shadow-xl shadow-md rounded-full px-8 py-2 text-xl font-medium text-white"> order now</button>
-                    </div>
-                    <div className="food-card bg-red-500/10  rounded-xl flex flex-col cursor-pointer items-center p-5">
-                        <div className="relative mb-3">
-                            <img src="https://i.ibb.co/jMxb2GC/i5.png" alt="" srcset="" />
-                            <div className="absolute top-2 left-2">
-                                <button className="shadow-sm text-white bg-red-500 hover:bg-red-700 cursor-pointer p-5 rounded-full relative">
-                                    <FaHeart className=' absolute text-xl top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ' />
-                                </button>
-                            </div>
-
-                            <div className=" absolute bottom-2 right-2">
-                                <button className=" shadow-sm bottom-4 border-white text-white bg-[#fdc55e] cursor-pointer p-3 h-14 w-14 text-xl font-bold rounded-full relative">
-                                    <div className=" absolute text-xl top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">$10</div>
-                                </button>
-                            </div>
-                        </div>
-                        <div className="flex gap-4 items-center">
-                            <p className="text-xl text-center font-bold text-[#f54748]">
-                                Kebab
-                            </p>
-                            <div className="flex text-sm space-x-2 cursor-pointer">
-                                <span className=" font-normal text-[#fdc55e]">4.3</span>
-                                <FaStar size={16} className='text-[#fdc55e]' />
-                                <span className=" font-medium">(4)</span>
-                            </div>
-
-                        </div>
-                        <button className=" bg-[#f54748] active:scale-90 transition duration-150 transform hover:shadow-xl shadow-md rounded-full px-8 py-2 text-xl font-medium text-white"> order now</button>
-                    </div>
-                    <div className="food-card bg-red-500/10  rounded-xl flex flex-col cursor-pointer items-center p-5">
-                        <div className="relative mb-3">
-                            <img src="https://i.ibb.co/jMxb2GC/i5.png" alt="" srcset="" />
-                            <div className="absolute top-2 left-2">
-                                <button className="shadow-sm text-white bg-red-500 hover:bg-red-700 cursor-pointer p-5 rounded-full relative">
-                                    <FaHeart className=' absolute text-xl top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ' />
-                                </button>
-                            </div>
-
-                            <div className=" absolute bottom-2 right-2">
-                                <button className=" shadow-sm bottom-4 border-white text-white bg-[#fdc55e] cursor-pointer p-3 h-14 w-14 text-xl font-bold rounded-full relative">
-                                    <div className=" absolute text-xl top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">$10</div>
-                                </button>
-                            </div>
-                        </div>
-                        <div className="flex gap-4 items-center">
-                            <p className="text-xl text-center font-bold text-[#f54748]">
-                                Kebab
-                            </p>
-                            <div className="flex text-sm space-x-2 cursor-pointer">
-                                <span className=" font-normal text-[#fdc55e]">4.3</span>
-                                <FaStar size={16} className='text-[#fdc55e]' />
-                                <span className=" font-medium">(4)</span>
-                            </div>
-
-                        </div>
-                        <button className=" bg-[#f54748] active:scale-90 transition duration-150 transform hover:shadow-xl shadow-md rounded-full px-8 py-2 text-xl font-medium text-white"> order now</button>
-                    </div>
+                    {
+                        ratedfood?.map(curElem => <Foods curElem={curElem} />)
+                    }
                 </div>
             </div>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import { UserProvider } from '../context/userContext.jsx'
+import { FoodProvider } from '../context/foodContext.jsx'
+import { CartProvider } from '../context/cardContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <UserProvider>
+
+        <FoodProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FoodProvider>
+      </UserProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
